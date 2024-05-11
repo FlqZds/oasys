b6bc342c58ae9bec737d578753104162c6d2b4c4
FlqZds <114152389+FlqZds@users.noreply.github.com>
2024-05-11 15:45:50 +0800

    Add files via upload

---
+++ oasys.sql
@@ -0,0 +1,73 @@
+/*
+SQLyog Community v12.09 (64 bit)
+MySQL - 8.0.36 : Database - oas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`oas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `oas`;
+
+/*Table structure for table `log` */
+
+DROP TABLE IF EXISTS `log`;
+
+CREATE TABLE `log` (
+  `user_id` int NOT NULL COMMENT '用户id',
+  `log_id` int NOT NULL AUTO_INCREMENT COMMENT '日志id',
+  `log_content` varchar(4096) NOT NULL COMMENT '日志内容',
+  `log_time` timestamp NOT NULL COMMENT '创建时间',
+  `role_id` int NOT NULL COMMENT '权限id',
+  PRIMARY KEY (`user_id`),
+  KEY `log_id` (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `log` */
+
+insert  into `log`(`user_id`,`log_id`,`log_content`,`log_time`,`role_id`) values (1,1,'test测试','2024-04-24 14:43:57',0);
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `user_id` int NOT NULL COMMENT '用户id',
+  `user_name` varchar(255) NOT NULL COMMENT '用户名',
+  `role_id` int NOT NULL COMMENT '权限id',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `role` */
+
+insert  into `role`(`user_id`,`user_name`,`role_id`) values (1,'admin',0);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `user_name` varchar(255) NOT NULL COMMENT '用户名',
+  `user_count` varchar(255) NOT NULL COMMENT '用户账号',
+  `password` varchar(255) NOT NULL COMMENT '密码',
+  `user_role` int NOT NULL COMMENT '用户权限',
+  `create_time` timestamp NOT NULL COMMENT '用户创建时间',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`user_name`,`user_count`,`password`,`user_role`,`create_time`) values (1,'admin','admin','123456',0,'2024-04-24 14:43:01');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
